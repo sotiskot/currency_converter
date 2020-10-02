9a0165bae1900e477aa00aca1d2c17f9c81c6064
ChubbyFatBoy <sotisskot@gmail.com>
2020-10-02 16:27:47 +0300

    setting up tools

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CurrencyController extends AbstractController
+{
+    /**
+     * @Route("/currency", name="currency")
+     */
+    public function index()
+    {
+        return $this->render('currency/index.html.twig', [
+            'controller_name' => 'CurrencyController',
+        ]);
+    }
+}
